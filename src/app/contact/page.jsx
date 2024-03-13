8b23912d83de2faaca3710b6666c5157b926365c
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-03-12 18:10:23 -0600

    Improved details in contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -32,10 +32,10 @@ const form = useRef();
   };
 
  return (
-    <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
+    <motion.div className="h-full overflow-scroll lg:overflow-hidden xl:overflow-hidden" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-3xl lg:text-6xl xl:text-6xl">
             <div className="">{text.split("").map((letter, index)=>(
                 <motion.span key={index} initial={{opacity:1}} animate={{opacity:0}} transition={{duration:3, repeat:Infinity, delay:index*0.1}}>{letter}</motion.span>
             ))}😊</div>
@@ -43,15 +43,15 @@ const form = useRef();
         {/* FORM CONTAINER */}
         <form
             onSubmit={sendEmail} 
-            ref={form} className="h-1/2 lg:h-full lg:w-1/2 bg-purple-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
-            <span>Dear Karen,</span>
-            <textarea rows={6} className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            ref={form} className="h-[80%] lg:h-full lg:w-1/2 bg-purple-50 rounded-xl text-lg lg:text-xl xl:text-xl flex flex-col gap-8 justify-center p-10 sm:p-18 md:p-24 lg:p-24 xl:p-24">
+            <span className="pt-5">Dear Karen,</span>
+            <textarea rows={10} className="bg-transparent border-b-2 border-b-black outline-none resize-none min-h-[2rem]"
             name="user_message"
             />
             <span>My mail adress is:</span>
             <input name="user_email" type="text" className="bg-transparent border-b-2 border-b-black outline-none" />
             <span>Regards.</span>
-            <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">Send</button>
+            <button className="bg-purple-200 rounded font-semibold text-gray-600 lg:p-4 xl:p-4">Send</button>
             {success && <span className="text-green-600 font-semibold">Your message has been sent succesfully!</span>}
             {error && <span className="text-red-600 font-semibold">Something went wrong!</span>}
         </form>
